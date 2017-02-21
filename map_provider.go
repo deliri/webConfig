795d6c82b3198026039980417c2e65fdf7d8d26f
Ase Deliri <deliri.ase@gmail.com>
2017-02-21 12:57:22 -0500

    fixed map prodiver

--- map_provider.go
+++ map_provider.go
@@ -1,12 +1,12 @@
-package cfg 
+package cfg
 
-// MapProvider provides a simple implementation of the provider whereby it 
-// just returns a stored map 
-type MapProvider struct{
-    Map map[string]string
+// MapProvider provides a simple implementation of the Provider whereby it just
+// returns a stored map.
+type MapProvider struct {
+	Map map[string]string
 }
 
-// Provide implements the provider interface
-func  (mp MapProvider)Provide()(map[string]string, error) {
-    return mp.Map, nil 
+// Provide implements the Provider interface.
+func (mp MapProvider) Provide() (map[string]string, error) {
+	return mp.Map, nil
 }
